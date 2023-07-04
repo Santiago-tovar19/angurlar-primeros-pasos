35e782c417cb41111efe9d2c8a3a2fd770b98367
Santiago Tovar <santiagocentenot@gmail.com>
2023-07-04 16:59:35 -0400

    Primeros pasos

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,25 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css'],
 })
-export class AppComponent {
+export class CounterComponent {
   public counter: number = 10;
 
   increaseOne() {
     this.counter = this.counter + 1;
   }
-
   decreaseOne() {
     this.counter = this.counter - 1;
   }
-
   increaseBy(value: number) {
     this.counter = this.counter + value;
   }
-
   decreaseBy(value: number) {
     this.counter = this.counter - value;
   }
