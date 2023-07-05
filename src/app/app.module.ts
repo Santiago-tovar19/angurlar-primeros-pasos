087c96e6f2d01802ef18b90894bd3ccfc48131c8
José Andrés <joseandreshernandezross@gmail.com>
2023-07-05 15:03:58 -0400

    Cambios realizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,29 @@ import { CounterModule } from './counter/counter.module';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormulariosModule } from './formularios/formularios.module';
+import { Route, RouterModule } from '@angular/router';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { FormulariosComponent } from './formularios/formularios.component';
 
+
+const routes: Route[] = [
+  {
+    path: 'pokemon',
+    // loadChildren: () => import('./pokemon/pokemon.module').then(m => m.PokemonModule)
+    component: PokemonComponent
+  },
+  {
+    path: 'formularios',
+    component: FormulariosComponent
+  }
+]
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     BrowserAnimationsModule,
     CounterModule,
     PokemonModule,
--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonComponent } from './pokemon.component';
+import { Route, RouterModule } from '@angular/router';
 
 
+// const route: Route[] = [
+//   {
+//     path: '',
+//     component: PokemonComponent
+//   }
+// ]
 
 @NgModule({
   declarations: [
     PokemonComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    // RouterModule.forChild(route)
   ],
   exports: [
     PokemonComponent
